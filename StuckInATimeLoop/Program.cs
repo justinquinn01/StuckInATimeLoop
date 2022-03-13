2132197a0abda862ed1d863500488171b3bd8c80
twist <twist@BLD>
2022-03-13 14:29:39 -0400

    Add project files.

---
+++ StuckInATimeLoop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace StuckInATimeLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= x; i++)
+                {
+                    Console.Write(i + " Abracadabra\n");
+                }
+        }
+    }
+}
